39e9b051769dfb4b3eb82b82dfea19db0eebd2fd
Carson Zhang <carsonzhang4@gmail.com>
2024-09-01 19:20:07 +0200

    renamed example script

--- R/example_callback_with_helper.R
+++ R/example_callback_with_helper.R
@@ -9,17 +9,17 @@ cbs_hist = tc_hist$generator$new()
 custom_logger = torch_callback("custom_logger",
     initialize = function(alpha = 0.1) {
         self$alpha = alpha
-        self$moving_loss = NULL
+        self$last_loss = NULL
     },
     on_batch_end = function() {
         if (is.null(self$moving_training_loss)) {
             self$moving_loss = self$ctx_last_loss
         } else {
-            self$moving_loss = self$alpha * self$last_loss + (1 - self$alpha) * self$moving_loss
+            self$moving_loss = self$alpha * self$ctx_last_loss + (1 - self$alpha) * self$moving_loss
         }
     },
     on_before_valid = function() {
-        cat(sprintf("Epoch %s: %.2f\n", self$ctx$epoch,))
+        cat(sprintf("Epoch %s: %.2f\n", self$ctx$epoch, self$ctx$moving_loss))
     },
     state_dict = function() {
         self$moving_loss
@@ -35,4 +35,4 @@ mlp = lrn("classif.mlp", callbacks = custom_logger, cb.custom_logger.alpha = 0.0
 mlp$train(task)
 
 # view the info in the state_dict, i.e. the final moving loss
-mlp$model$callbacks$custom_logger
+print(mlp$model$callbacks$custom_logger)
