dc26ea48e36163919b33ab04dd8afbb78760428f
Carson Zhang <carsonzhang4@gmail.com>
2024-09-07 15:41:03 +0200

    simple working version for one measure

--- R/example-tfevents.R
+++ R/example-tfevents.R
@@ -2,10 +2,10 @@ library(tfevents)
 library(torch)
 library(fs)
 
-epochs = 10
+epochs <- 10
 for (i in seq_len(epochs)) {
   log_event(
-    train = list(loss =runif(1), acc = runif(1)),
+    train = list(loss = runif(1), acc = runif(1)),
     valid = list(loss = runif(1), acc = runif(1))
   )
 }
@@ -13,3 +13,4 @@ for (i in seq_len(epochs)) {
 fs::dir_tree("logs")
 
 tensorflow::tensorboard(normalizePath("logs"), port = 6060)
+
--- R/tfevents-custom_callback-helper.R
+++ R/tfevents-custom_callback-helper.R
@@ -1,28 +1,52 @@
 library(mlr3torch)
 library(tfevents)
+library(fs)
 
-custom_tf_logger = torch_callback("custom_tf_logger",
-  initialize = function() {
-    self$last_train_loss = NULL
-    # self$last_valid_loss = NULL
+# custom_tf_logger = torch_callback("custom_tf_logger",
+#   initialize = function() {
+#     self$last_train_loss = NULL
+#   },
+#   on_batch_end = function() {
+#     self$last_train_loss = self$ctx$last_loss
+#     log_event(train_loss = self$last_train_loss)
+#   },
+#   state_dict = function() {
+#     self$last_train_loss
+#   },
+#   load_state_dict = function(state_dict) {
+#     self$last_train_loss = state_dict
+#   }
+# )
+
+# TODO: extend to multiple measures
+custom_tf_logger_valid = torch_callback("custom_tf_logger",
+  on_batch_end = function() {
+    if (length(self$ctx$last_scores_train) > 0) {
+      log_event(train_measure = self$ctx$last_scores_train[[names(self$ctx$measures_train)]])
+    }
+    print(self$ctx$last_scores_train)
   },
-  on_epoch_end = function() {
-    log_event(train_loss = list(loss = self$ctx$last_loss))
-  },
-  state_dict = function() {
-    self$last_train_loss
-  },
-  load_state_dict = function(state_dict) {
-    self$last_train_loss = state_dict
+  on_batch_valid_end = function() {
+    if (length(self$ctx$last_scores_valid) > 0) {
+      log_event(valid_measure = self$ctx$last_scores_valid[[names(self$ctx$measures_valid)]])
+    }
+    print(self$ctx$last_scores_valid)
   }
 )
 
 task = tsk("iris")
-task$divide(0.3)
 
-mlp = lrn("classif.mlp", callbacks = custom_tf_logger, epochs = 5, batch_size = 64, neurons = 20)
+mlp = lrn("classif.mlp", 
+          callbacks = custom_tf_logger_valid,
+          epochs = 5, batch_size = 64, neurons = 20,
+          validate = 0.2, measures_valid = msrs(c("classif.acc")), measures_train = msrs(c("classif.acc"))
+      )
 
 mlp$train(task)
-print(mlp$model$callbacks$custom_logger)
+# print(mlp$model$callbacks$custom_tf_logger)
 
+fs::dir_tree("logs")
 
+tensorflow::tensorboard(normalizePath("logs"), port = 6060)
+
+# need to restart R session to "start from scratch"
